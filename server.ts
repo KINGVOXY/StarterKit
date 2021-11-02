7bf81b105390ffd9decb801658970531333a0b49
KINGVOXY <daruo0830@gmail.com>
2021-11-02 22:19:53 +0900

    assetsパスはまとめてserverに記述

--- server.ts
+++ server.ts
@@ -1,10 +1,13 @@
 import { System, Config } from "https://raw.githubusercontent.com/PuddleServer/Puddle/develop/mod.ts";
-import { routes } from "./config/routes.ts"
-import { assets } from "./config/assets.ts"
+import { routes } from "./config/routes.ts";
 
-
+// ルーティング
+// routing
 System.createRoutes(...routes);
-System.createRoutes(...assets);
+
+// 素材の読み込み
+// assets importing
+System.createRoutes("./views/assets/*");
 
 System.listen("./config/.env", (conf: Config)=>{
     console.log(`The server running on http://${conf.SERVER.HOSTNAME}:${conf.SERVER.PORT}`);
